1cecfe248dab723ffaae70b168ce4c1b745c12da
Dali_burjaliani <72936604+daliburjaliani@users.noreply.github.com>
2021-08-15 23:26:18 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET"])
-def entry():
-    return render_template("entry.html")
-
-
-@app.route('/result', methods=["POST"])
-def result():
-    city = request.form['city']
-    owm = OWM('5dfdd607d0c33f12e428ca24dce76755')
-    mgr = owm.weather_manager()
-
+def weather(city):
     try:
+        owm = OWM('5dfdd607d0c33f12e428ca24dce76755')
+        mgr = owm.weather_manager()
+
         obs = mgr.weather_at_place(city)
         w = obs.weather
 
@@ -30,16 +23,31 @@ def result():
 
         min_max = f"{max_temp}° - {min_temp}°"
 
-        return render_template("result.html",
-                               city=city.title(),
-                               status=status.title(),
-                               wind=wind,
-                               humidity=humidity,
-                               temp=temp,
-                               temp_min_max=min_max)
+        return status, wind, humidity, temp, min_max
+
     except (pyowm.commons.exceptions.NotFoundError, pyowm.commons.exceptions.APIRequestError):
         return render_template("result.html",
                                city="Not Found")
+
+@app.route('/', methods=["GET"])
+def entry():
+    return render_template("entry.html")
+
+
+@app.route('/result', methods=["POST"])
+def result():
+    city = request.form['city']
+
+    status, wind, humidity, temp, min_max = weather(city)
+
+
+    return render_template("result.html",
+                           city=city.title(),
+                           status=status.title(),
+                           wind=wind,
+                           humidity=humidity,
+                           temp=temp,
+                           temp_min_max=min_max)
 
 
 if __name__ == '__main__':
